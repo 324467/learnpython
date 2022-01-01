094459bf18582e1cf7997202af7403b2812bd2b7
ezlio <1217372635@qq.com>
2022-01-01 23:12:43 +0800

    pull test

---
+++ test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def mul(x, *y):
+    sum = x
+    for n in y:
+        sum = sum * n
+    return sum
+
+
+print('mul(5) =', mul(5))
+print('mul(5, 6) =', mul(5, 6))
+print('mul(5, 6, 7) =', mul(5, 6, 7))
+print('mul(5, 6, 7, 9) =', mul(5, 6, 7, 9))
+if mul(5) != 5:
+    print('测试失败!')
+elif mul(5, 6) != 30:
+    print('测试失败!')
+elif mul(5, 6, 7) != 210:
+    print('测试失败!')
+elif mul(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        mul()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
